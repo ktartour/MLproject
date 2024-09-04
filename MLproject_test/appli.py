aa67b7c45d70e038547dfe2ef947d6714e3ff1ca
kevin <kevin.tartour@gmail.com>
2024-09-04 11:12:25 +0200

    Creation_git

---
+++ MLproject_test/appli.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from sections.classification.classification import classification_page
+from sections.nailsdetection.nails import nail_page
+from sections.regression.regression import regression_page
+
+st.set_page_config(
+    page_title="Playground ML",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+type_data = st.sidebar.radio(
+    "Choisissez votre type de playground",
+    ["Regression", "Classification", "NailsDetection"]
+)
+
+if type_data == "Regression":
+    regression_page()
+elif type_data == "Classification":
+    classification_page()
+elif type_data == "NailsDetection":
+    nail_page()
+else:
+    st.write("Choisissez une option")
